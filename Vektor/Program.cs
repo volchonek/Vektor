a65434b8698e975ac490c2d4cad005ce1a4add7c
volchonek <volchonek@gmail.com>
2016-06-09 00:12:11 +0300

    Add generic type and generator coda

--- Vektor/Program.cs
+++ Vektor/Program.cs
@@ -7,10 +7,11 @@ using System.Threading.Tasks;
 
 namespace Vektor
 {
-	class Rainbow : IComparable, IEquatable<Rainbow>
+	class Rainbow<T> : IComparable<Rainbow<T>>, IEquatable<Rainbow<T>> where T : struct
 	{
-		protected string Name { get; set; }
-		protected int Lenght { get; set; }
+		public T Value { get; set; }
+		public string Name { get; set; }
+		public int Lenght { get; set; }
 		public Rainbow() { }
 		public Rainbow(string Name, int Lenght)
 		{
@@ -22,15 +23,15 @@ namespace Vektor
 			Console.WriteLine("Цвет - {0}, длинна волны - {1} н.м.", this.Name, this.Lenght);
 		}
 
-		public int CompareTo(object obj)
+		public int CompareTo(Rainbow<T> obj)
 		{
-			Rainbow color = (Rainbow)obj;
+			Rainbow<T> color = (Rainbow<T>)obj;
 			if (this.Lenght > color.Lenght) return 1;
 			if (this.Lenght < color.Lenght) return -1;
 			return 0;
 		}
 
-		public bool Equals(Rainbow other)
+		public bool Equals(Rainbow<T> other)
 		{
 			if (ReferenceEquals(null, other))
 			{
@@ -43,14 +44,22 @@ namespace Vektor
 			return this.Lenght == other.Lenght && int.Equals(this.Lenght, other.Lenght);
 		}
 	}
-	//класс реализующий генератор векторов
-	class GeneratorCodeRainbow : IEnumerable<int>
+	//класс реализующий генератор векторов ДОДЕЛАТЬ
+	class GeneratorCodeRainbow<T> : IEnumerable<Rainbow<T>> where T : struct
 	{
-		public IEnumerator<int> GetEnumerator()
+		public GeneratorCodeRainbow(int count, int start = 0)
 		{
-			for (int i = 380; i <= 780; i = i + 50)
+			this.Start = start;
+			this.Count = count;
+		}
+		public string c { get; set; }
+		public int Start { get; private set; }
+		public int Count { get; private set; }
+		public IEnumerator<Rainbow<T>> GetEnumerator()
+		{
+			for (int i = Start; i < Start + Count; i++)
 			{
-				yield return i;
+				yield return new Rainbow<T>(c, i);
 			}
 		}
 
@@ -64,20 +73,21 @@ namespace Vektor
 	{
 		static void Main(string[] args)
 		{
-			var listcolor = new List<Rainbow>();
+			var listcolor = new List<Rainbow<int>>();
 
-			listcolor.Add(new Rainbow("Оранжевый", 605));
-			listcolor.Add(new Rainbow("Зеленный", 530));
-			listcolor.Add(new Rainbow("Голубой", 495));
-			listcolor.Add(new Rainbow("Синий", 465));
-			listcolor.Add(new Rainbow("Красный", 700));
-			listcolor.Add(new Rainbow("Желтый", 580));
-			listcolor.Add(new Rainbow("Фиалетовый", 415));
+			listcolor.Add(new Rainbow<int>("Оранжевый", 605));
+			listcolor.Add(new Rainbow<int>("Зеленный", 530));
+			listcolor.Add(new Rainbow<int>("Голубой", 495));
+			listcolor.Add(new Rainbow<int>("Синий", 465));
+			listcolor.Add(new Rainbow<int>("Красный", 700));
+			listcolor.Add(new Rainbow<int>("Желтый", 580));
+			listcolor.Add(new Rainbow<int>("Фиалетовый", 415));
 			foreach (var item in listcolor)
 			{
 				item.Draw();
 			}
 			//сортировка через IComparable
+			Console.WriteLine(" ");
 			listcolor.Sort();
 			Console.WriteLine("________________________________________");
 			foreach (var item in listcolor)
@@ -86,10 +96,10 @@ namespace Vektor
 			}
 			// сравнение цветов
 			Console.WriteLine("________________________________________");
-			var red = new Rainbow("Красный", 700);
-			var blue = new Rainbow("Синий", 465);
-
-			if (blue.Equals(red))
+			var color1 = new Rainbow<int>("Красный", 700);
+			var color2 = new Rainbow<int>("Синий", 465);
+			Console.WriteLine("Сравнение {0} и {1}", color1.Name, color2.Name);
+			if (color1.Equals(color2))
 			{
 				Console.WriteLine("Цвета совпадают");
 			}
@@ -99,21 +109,43 @@ namespace Vektor
 			}
 			//повторное сравнение цветов для проверки функции Equals
 			Console.WriteLine("________________________________________");
-			var red1 = new Rainbow("Красный", 700);
-			var red2 = new Rainbow("Красный", 700);
-
-			if (red1.Equals(red2))
+			var color3 = new Rainbow<int>("Красный", 700);
+			var color4 = new Rainbow<int>("Красный", 700);
+			Console.WriteLine("Сравнение {0} и {1}", color3.Name, color4.Name);
+			if (color3.Equals(color4))
 			{
 				Console.WriteLine("Цвета совпадают");
 			}
 			else
 			{
 				Console.WriteLine("Цвета не совпадают");
-			}			
+			}
+			Console.WriteLine("________________________________________");
+			Console.WriteLine(" ");
+			Console.WriteLine("Генератор кода");
+			Console.WriteLine(" ");
+			//Генератор кода Радуги из новых цветов с возможностью указания длинный волны в методе генератора
+			var lc = GetRainbowSimple();
+			foreach (var item in lc)
+			{
+				item.Draw();
+			}
 			Console.ReadKey();
 		}
-		public static void Sets<T>(T item)
+		/// <summary>
+		/// Метод заполняет список из объектов класса Rainbow. Метод использует тип double.
+		/// </summary>
+		/// <param name="start">начальный диапазон волны</param>
+		/// <param name="count">конечный диапазон волны</param>
+		/// <returns></returns>
+		static IEnumerable<Rainbow<double>> GetRainbowSimple(int start = 415, int count = 700)
 		{
+			var results = new List<Rainbow<double>>();
+			for (int i = start; i < count; i = i + 50)
+			{
+				results.Add(new Rainbow<double>("length color " + i, i));
+			}
+			return results;
 		}
 	}
 }
